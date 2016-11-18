da00bd8b29228643c333612c148dc8320f78b577
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-11-17 21:09:39 -0800

    why did i rewrite that for no reason

--- src/main/java/tehnut/versionproxy/one_eleven/WorldHelper.java
+++ src/main/java/tehnut/versionproxy/one_eleven/WorldHelper.java
@@ -1,12 +1,8 @@
 package tehnut.versionproxy.one_eleven;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import tehnut.versionproxy.iface.IWorldHelper;
@@ -21,8 +17,6 @@ public class WorldHelper implements IWorldHelper {
 
     @Override
     public boolean canBlockBePlaced(World world, Block block, BlockPos pos, boolean ignoreCollision, EnumFacing side, @Nullable Entity entity) {
-        IBlockState state = world.getBlockState(pos);
-        AxisAlignedBB axisalignedbb = ignoreCollision ? null : block.getDefaultState().getCollisionBoundingBox(world, pos);
-        return !(axisalignedbb != Block.NULL_AABB && !world.checkNoEntityCollision(axisalignedbb.offset(pos), entity)) && (state.getMaterial() == Material.CIRCUITS && state.getBlock() == Blocks.ANVIL || state.getBlock().isReplaceable(world, pos) && state.getBlock().canPlaceBlockOnSide(world, pos, side));
+        return world.func_190527_a(block, pos, ignoreCollision, side, entity);
     }
 }
